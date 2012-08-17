10dfa2e0b1baff1719a80032bf78e76ef7b7b1a2
Namhoon Kim <yanhkim@gmail.com>
2012-08-17 15:26:13 +0900

    before scary unlink experiment

---
+++ ch13/ex13-3.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+
+chomp (my $dir = <STDIN>);
+
+if ($dir) {
+    chdir $dir or die "$! - $dir\n";
+} else {
+    chdir;
+}
+
+print join("  ", sort(glob "* .*")), "\n";
